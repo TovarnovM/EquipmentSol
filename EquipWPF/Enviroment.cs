77dc2d3a8e34284de7729909fa39e7cd44f03b53
Михаил Товарнов <misha198921rip@gmail.com>
2016-10-03 14:40:49 +0300

    ыы

---
+++ EquipWPF/Enviroment.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EquipWPF {
+    public interface IUnit {
+        Enviroment Owner { get; set; }
+        string Name { get; set; }
+        void Update();
+        bool Enabled { get; set; }
+        bool Dead { get; set; }
+        Vector Pos { get; }
+    }
+
+    public class Enviroment {
+        public List<IUnit> Units { get; private set; } = new List<IUnit>();
+        public double Time { get; set; }
+        public double dT { get; set; } = 0.0001;
+        public Func<bool> StopFunc { get; set; }
+        public double MaxTime { get; set; } = 100d;
+
+        public Enviroment() {
+            StopFunc += () => {
+                return Time >= MaxTime;
+            };
+        }
+        public void AddUnit(IUnit unit) {
+            Units.Add(unit);
+            unit.Owner = this;
+        }
+        public void UpdateAllUnits() {
+            foreach(var unit in Units.Where(u=>u.Enabled)) {
+                unit.Update();
+            }
+        }
+        public void EnableAllUnits() {
+            Units.ForEach(u => u.Enabled = true);
+        }
+        public void Start() {
+            Time = 0d;
+            while(true) {
+                UpdateAllUnits();
+                if(StopFunc())
+                    break;
+                Time += dT;
+            }
+        }
+    }
+}
